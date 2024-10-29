07395a9402b4ed118c2b109bd867e82280627715
jmaloon <joshmaloon@gmail.com>
2024-10-28 23:38:05 -0600

    add fontawesome icons

--- src/app/ThemeSwitch.tsx
+++ src/app/ThemeSwitch.tsx
@@ -4,6 +4,9 @@ import { Switch } from "@headlessui/react";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
+
 export default function ThemeSwitch() {
   const [isMounted, setIsMounted] = useState(false);
   const { resolvedTheme, setTheme } = useTheme();
@@ -17,12 +20,16 @@ export default function ThemeSwitch() {
   const isDark = resolvedTheme === "dark";
 
   return (
-    <Switch
-      checked={isDark}
-      onChange={() => setTheme(isDark ? "light" : "dark")}
-      className="group inline-flex h-6 w-11 items-center rounded-full bg-gray-200 transition data-[checked]:bg-blue-600"
-    >
-      <span className="size-4 translate-x-1 rounded-full bg-white transition group-data-[checked]:translate-x-6" />
-    </Switch>
+    <div className="flex gap-2 items-center">
+      <FontAwesomeIcon icon={faSun} />
+      <Switch
+        checked={isDark}
+        onChange={() => setTheme(isDark ? "light" : "dark")}
+        className="group inline-flex h-6 w-11 items-center rounded-full bg-gray-200 transition data-[checked]:bg-blue-600"
+      >
+        <span className="size-4 translate-x-1 rounded-full bg-white transition group-data-[checked]:translate-x-6" />
+      </Switch>
+      <FontAwesomeIcon icon={faMoon} />
+    </div>
   );
 }
