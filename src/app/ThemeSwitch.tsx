463f9c0f3918c95849d4c7632b30848b5b557b80
jmaloon <joshmaloon@gmail.com>
2024-10-28 23:41:25 -0600

    add contrast for switch in light theme

--- src/app/ThemeSwitch.tsx
+++ src/app/ThemeSwitch.tsx
@@ -25,7 +25,7 @@ export default function ThemeSwitch() {
       <Switch
         checked={isDark}
         onChange={() => setTheme(isDark ? "light" : "dark")}
-        className="group inline-flex h-6 w-11 items-center rounded-full bg-gray-200 transition data-[checked]:bg-blue-600"
+        className="group inline-flex h-6 w-11 items-center rounded-full bg-gray-300 transition data-[checked]:bg-blue-600"
       >
         <span className="size-4 translate-x-1 rounded-full bg-white transition group-data-[checked]:translate-x-6" />
       </Switch>
