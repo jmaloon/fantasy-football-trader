200346d80b30b682f9906516fa0f98b72653b4b1
jmaloon <joshmaloon@gmail.com>
2024-10-29 22:30:01 -0600

    placeholder switch to prevent layout shift

--- src/app/ThemeSwitch.tsx
+++ src/app/ThemeSwitch.tsx
@@ -15,21 +15,23 @@ export default function ThemeSwitch() {
     setIsMounted(true);
   }, []);
 
-  if (!isMounted) return null;
-
   const isDark = resolvedTheme === "dark";
 
   return (
     <div className="flex gap-2 items-center">
-      <FontAwesomeIcon icon={faSun} />
-      <Switch
-        checked={isDark}
-        onChange={() => setTheme(isDark ? "light" : "dark")}
-        className="group inline-flex h-6 w-11 items-center rounded-full bg-gray-300 transition data-[checked]:bg-blue-600"
-      >
-        <span className="size-4 translate-x-1 rounded-full bg-white transition group-data-[checked]:translate-x-6" />
-      </Switch>
-      <FontAwesomeIcon icon={faMoon} />
+      <FontAwesomeIcon icon={faSun} className="h-4" />
+      {isMounted ? (
+        <Switch
+          className="group inline-flex h-6 w-11 items-center rounded-full bg-gray-300 transition data-[checked]:bg-blue-600"
+          checked={isDark}
+          onChange={() => setTheme(isDark ? "light" : "dark")}
+        >
+          <span className="size-4 translate-x-1 rounded-full bg-white transition group-data-[checked]:translate-x-6" />
+        </Switch>
+      ) : (
+        <div className="group inline-flex h-6 w-11 items-center rounded-full bg-gray-300 transition data-[checked]:bg-blue-600"></div>
+      )}
+      <FontAwesomeIcon icon={faMoon} className="h-4" />
     </div>
   );
 }
